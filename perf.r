c89e0946ea8b6f5e55d6da6e623a99ea0e13f388
Aron Novak <aaron@szentimre.hu>
2013-08-11 12:28:49 +0200

    perf.r, visualize.sh: improve the generated chart

--- perf.r
+++ perf.r
@@ -1,6 +1,12 @@
-files <- list.files(path=".", pattern="*dat")
+# Visualizes multiple ApacheBench output files in one chart.
+#
+# Drupalaton 2013
 
-# series <- c()
+# TODO(aaron): Achieve full conformance to
+# http://google-styleguide.googlecode.com/svn/trunk/Rguide.xml
+
+# Looping through the data files and read all the values.
+files <- list.files(path=".", pattern="*dat")
 
 for (file in files) {
   data <- read.delim(file)
@@ -12,11 +18,24 @@ for (file in files) {
   }
 }
 
+# Orders it chronologically.
 attach(series)
 ordered_series <- series[order(seconds),]
 
-ordered_series
+# Opens the output image.
+png(
+  paste('perf-', as.integer(Sys.time()), '.png', sep=''),
+  width=700,
+  height=700
+)
 
-png('perf.png')
-plot(ordered_series[-1 * length(ordered_series),5])
+# Creates the chart, the total loading time is at the Y axis, the Unix
+# timestamp is at the X axis.
+plot(
+  ordered_series[-1 * length(ordered_series),2],
+  ordered_series[-1 * length(ordered_series),5],
+  xlab="Unix timestamp",
+  ylab="Page load time [ms]",
+  main="Subsequent loading time measurements of the website"
+)
 dev.off()
--- visualize.sh
+++ visualize.sh
@@ -1,1 +1,8 @@
-R < perf.r  --no-save
+#!/bin/bash
+# Ivokes the measurement visualization R script.
+if ls *dat &> /dev/null;
+then
+  R -q < perf.r  --no-save
+else
+  echo "Please generate the .dat files with measure.sh first"
+fi
